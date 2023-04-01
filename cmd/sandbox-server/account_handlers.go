b3e517be3c9e3d9e15ba2a3d6fed7858ca656616
Guillaume Coré <gucore@redhat.com>
2023-04-01 12:23:37 +0200

    Add missing handlers

---
+++ cmd/sandbox-server/account_handlers.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/rhpds/sandbox/internal/api/v1"
+	sandboxdb "github.com/rhpds/sandbox/internal/dynamodb"
+	"github.com/rhpds/sandbox/internal/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rhpds/sandbox/internal/log"
+)
+
+type AccountHandler struct {
+	accountRepo models.AwsAccountRepository
+}
+
+func NewAccountHandler(accountRepo models.AwsAccountRepository) *AccountHandler {
+	return &AccountHandler{
+		accountRepo: accountRepo,
+	}
+}
+
+// GetAccountsHandler returns all accounts
+// GET /accounts
+func (h *AccountHandler) GetAccountsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", " ")
+
+	accounts, err := h.accountRepo.GetAccounts()
+
+	if err != nil {
+		log.Logger.Error("GET accounts", "error", err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.Error{
+			Code:    500,
+			Message: "Error reading accounts",
+		})
+		return
+	}
+
+	// Print accounts using JSON
+	if err := enc.Encode(accounts); err != nil {
+		log.Logger.Error("GET accounts", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.Error{
+			Code:    500,
+			Message: "Error reading account",
+		})
+	}
+}
+
+
+// GetAccountHandler returns an account
+// GET /accounts/:account
+func (h *AccountHandler) GetAccountHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", " ")
+
+	// Grab the parameters from Params
+	accountName := p.ByName("account")
+
+	// Get the account from DynamoDB
+	sandbox, err := h.accountRepo.GetAccount(accountName)
+	if err != nil {
+		if err == sandboxdb.ErrAccountNotFound {
+			log.Logger.Warn("GET account", "error", err)
+			w.WriteHeader(http.StatusNotFound)
+			enc.Encode(v1.Error{
+				Code:    http.StatusNotFound,
+				Message: "Account not found",
+			})
+			return
+		}
+		log.Logger.Error("GET account", "error", err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.Error{
+			Code:    500,
+			Message: "Error reading account",
+		})
+		return
+	}
+	// Print account using JSON
+	if err := enc.Encode(sandbox); err != nil {
+		log.Logger.Error("GET account", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.Error{
+			Code:    500,
+			Message: "Error reading account",
+		})
+	}
+}
---
+++ cmd/sandbox-server/handlers.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/rhpds/sandbox/internal/api/v1"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/rhpds/sandbox/internal/log"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type BaseHandler struct {
+	dbpool *pgxpool.Pool
+	svc    *dynamodb.DynamoDB
+}
+
+func NewBaseHandler(svc *dynamodb.DynamoDB, dbpool *pgxpool.Pool) *BaseHandler {
+	return &BaseHandler{
+		svc:    svc,
+		dbpool: dbpool,
+	}
+}
+
+func GetPlacementsHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", " ")
+}
+
+func (h *BaseHandler) HealthHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", " ")
+
+	// Check the database connection
+	dbpoolErr := h.dbpool.Ping(r.Context())
+
+	// Check the DynamoDB connection
+	_, dynamodbErr := h.svc.ListTables(&dynamodb.ListTablesInput{})
+
+	if dbpoolErr != nil {
+		log.Logger.Error("Health check", "error", dbpoolErr)
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.HealthCheckResult{
+			Code:    500,
+			Message: "Error connecting to Postgresql",
+		})
+		return
+	}
+
+	if dynamodbErr != nil {
+		log.Logger.Error("Health check", "error", dynamodbErr)
+		w.WriteHeader(http.StatusInternalServerError)
+		enc.Encode(v1.HealthCheckResult{
+			Code:    500,
+			Message: "Error connecting to DynamoDB",
+		})
+		return
+	}
+
+	log.Logger.Info("Health check", "status", "OK")
+	w.WriteHeader(http.StatusOK)
+	enc.Encode(v1.HealthCheckResult{
+		Code: 200,
+		Message: "OK",
+	})
+}
