95c5ba5d62ef8f0f406b01c1dbc635156688a9c1
Guillaume Coré <gucore@redhat.com>
2024-05-28 16:18:33 +0200

    Add metrics for lifecycle instance events (#62)

--- cmd/sandbox-metrics/main.go
+++ cmd/sandbox-metrics/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	sandboxdb "github.com/rhpds/sandbox/internal/dynamodb"
-	"github.com/rhpds/sandbox/internal/log"
-	"github.com/rhpds/sandbox/internal/models"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	sandboxdb "github.com/rhpds/sandbox/internal/dynamodb"
+	"github.com/rhpds/sandbox/internal/log"
+	"github.com/rhpds/sandbox/internal/models"
 )
 
 var interval = 30 * time.Second
@@ -33,7 +37,7 @@ func serve() {
 	http.ListenAndServe(":2112", nil)
 }
 
-func createMetrics() {
+func createMetrics(dbPool *pgxpool.Pool) {
 
 	gaugeVec := promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -41,6 +45,14 @@ func createMetrics() {
 			Help: "Accounts in use",
 		},
 		[]string{"name", "status", "to_cleanup", "reservation"},
+	)
+
+	gaugeLifecycleEvents := promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "sandbox_lifecycle_instance_events",
+			Help: "Lifecycle events",
+		},
+		[]string{"resource_type", "region", "instance_type", "event_type"},
 	)
 
 	used := promauto.NewGauge(prometheus.GaugeOpts{
@@ -66,7 +78,9 @@ func createMetrics() {
 			// no filter, we grab all accounts at once, then we filter because the DB is not that big.
 			accounts, err := accountProvider.FetchAll()
 			if err != nil {
-				log.Err.Fatal(err)
+				log.Err.Println(err)
+				time.Sleep(interval)
+				continue
 			}
 			used.Set(float64(models.CountUsed(accounts)))
 			toCleanup.Set(float64(models.CountToCleanup(accounts)))
@@ -95,6 +109,68 @@ func createMetrics() {
 			time.Sleep(interval)
 		}
 	}()
+
+	if dbPool == nil {
+		return
+	}
+	go func() {
+		for {
+			events, err := GetLifecycleInstanceEvents(dbPool)
+			if err != nil {
+				log.Err.Println(err)
+				time.Sleep(interval)
+				continue
+			}
+			gaugeLifecycleEvents.Reset()
+			for _, event := range events {
+				gaugeLifecycleEvents.WithLabelValues(
+					event.ResourceType,
+					event.Region,
+					event.InstanceType,
+					event.EventType,
+				).Set(float64(event.Count))
+			}
+			time.Sleep(interval)
+		}
+	}()
+}
+
+type LifecycleEvent struct {
+	ResourceType string
+	Region       string
+	InstanceType string
+	EventType    string
+	Count        int
+}
+
+func GetLifecycleInstanceEvents(dbpool *pgxpool.Pool) ([]LifecycleEvent, error) {
+	rows, err := dbpool.Query(
+		context.Background(),
+		`SELECT resource_type,
+                event_data->>'region' as iregion,
+                event_data->>'instance_type' as itype,
+                event_type, count(*)
+         FROM lifecycle_events GROUP BY resource_type, iregion, itype, event_type;`,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	events := []LifecycleEvent{}
+
+	for rows.Next() {
+		var event LifecycleEvent
+		err := rows.Scan(&event.ResourceType, &event.Region, &event.InstanceType, &event.EventType, &event.Count)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
 }
 
 // Build info
@@ -103,6 +179,18 @@ var buildTime = "undefined"
 var buildCommit = "HEAD"
 
 func main() {
+	var dbPool *pgxpool.Pool
+	if os.Getenv("DATABASE_URL") != "" {
+		connStr := os.Getenv("DATABASE_URL")
+		var err error
+		dbPool, err = pgxpool.Connect(context.Background(), connStr)
+
+		if err != nil {
+			log.Err.Fatal(err)
+		}
+		defer dbPool.Close()
+	}
+
 	parseFlags()
 	log.InitLoggers(debugFlag, []slog.Attr{
 		slog.String("version", Version),
@@ -112,7 +200,7 @@ func main() {
 
 	sandboxdb.CheckEnv()
 
-	createMetrics()
+	createMetrics(dbPool)
 
 	serve()
 }
