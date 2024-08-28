e8a50758e61bd156e4c83db3ecec4d7703b0db2e
Kirill Malgichev <kmalgich@redhat.com>
2024-08-28 14:12:17 -0400

    Adding Mutex to the pool allocation

--- internal/models/azure_sandbox.go
+++ internal/models/azure_sandbox.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"sync"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -29,6 +30,8 @@ type AzureSandboxProvider struct {
 	azureClientId      string
 	azureSecret        string
 	azurePoolApiSecret string
+
+	poolMutex sync.Mutex
 }
 
 type AzureSandboxWithCreds struct {
@@ -82,6 +85,59 @@ func NewAzureSandboxProvider(
 	return provider, nil
 }
 
+func (a *AzureSandboxProvider) allocateSubscription() (string, error) {
+	SubscriptionNames := map[string]bool{}
+
+	// TODO: These names are hard codded and should be stored somewhere
+	// at the config file. So, the pool variable should be initialized using
+	// configuration, not generate on the fly.
+	for i := 1; i <= poolSize; i++ {
+		SubscriptionNames[fmt.Sprintf("%s%03d", poolPrefix, i)] = false
+	}
+
+	rows, err := a.dbPool.Query(
+		context.Background(),
+		`SELECT resource_data ->> 'subscription_name' FROM resources WHERE status = 'success'`)
+	if err != nil {
+		return "", fmt.Errorf("can't get retrieve about allocated pools")
+	}
+	defer rows.Close()
+
+	allocatedSubscriptions := []string{}
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			return "", fmt.Errorf("illegal pool name retrieved: %w", err)
+		}
+		allocatedSubscriptions = append(allocatedSubscriptions, name)
+	}
+	if err = rows.Err(); err != nil {
+		return "", fmt.Errorf("can't get allocated pool names: %w", err)
+	}
+
+	for _, name := range allocatedSubscriptions {
+		if _, exists := SubscriptionNames[name]; exists {
+			SubscriptionNames[name] = true
+		} else {
+			log.Logger.Warn("Incorrect pool name found", "warning", name)
+			continue
+		}
+	}
+
+	availableSubscriptions := make([]string, 0, len(SubscriptionNames))
+	for k, v := range SubscriptionNames {
+		if !v {
+			availableSubscriptions = append(availableSubscriptions, k)
+		}
+	}
+
+	if len(availableSubscriptions) == 0 {
+		return "", fmt.Errorf("no available pools")
+	}
+
+	return availableSubscriptions[rand.Intn(len(availableSubscriptions))], nil
+}
+
 func (a *AzureSandboxProvider) Request(
 	serviceUuid string,
 	annotations Annotations,
@@ -92,7 +148,6 @@ func (a *AzureSandboxProvider) Request(
 
 	azureSandbox := AzureSandboxWithCreds{
 		AzureSandbox: AzureSandbox{
-			//			SubscriptionId: "pool-00-31",
 			Name:        sandboxName,
 			Kind:        "AzureSandbox",
 			ServiceUuid: serviceUuid,
@@ -102,11 +157,27 @@ func (a *AzureSandboxProvider) Request(
 		Provider: a,
 	}
 
-	err := azureSandbox.Save()
+	a.poolMutex.Lock()
+
+	subscriptionName, err := a.allocateSubscription()
 	if err != nil {
+		a.poolMutex.Unlock()
+
+		log.Logger.Error("Error crating Azure sandbox", "error", err)
+		return AzureSandboxWithCreds{}, err // TODO: Or should we save it with error state?
+	}
+
+	azureSandbox.SubscriptionName = subscriptionName
+
+	err = azureSandbox.Save()
+	if err != nil {
+		a.poolMutex.Unlock()
+
 		log.Logger.Error("Error saving Azure sandbox", "error", err)
 		return AzureSandboxWithCreds{}, err
 	}
+
+	a.poolMutex.Unlock()
 
 	// Create the sandbox asynchronously
 	go azureSandbox.Create()
@@ -275,65 +346,6 @@ func (sb *AzureSandboxWithCreds) setStatus(status string) error {
 	return err
 }
 
-// TODO: I think this function should be part of the sb provider
-func (sb *AzureSandboxWithCreds) allocatePool() error {
-	pool := map[string]bool{}
-
-	// TODO: These names are hard codded and should be stored somewhere
-	// at the config file. So, the pool variable should be initialized using
-	// configuration, not generate on the fly.
-	for i := 1; i <= poolSize; i++ {
-		pool[fmt.Sprintf("%s%03d", poolPrefix, i)] = false
-	}
-
-	// TODO: Mutex is required here to properly alocate pool
-	// sb.mu.Lock()
-	// defer sb.mu.Unlock()
-
-	rows, err := sb.Provider.dbPool.Query(
-		context.Background(),
-		`SELECT resource_data ->> 'subscription_name' FROM resources WHERE status = 'success'`)
-	if err != nil {
-		return fmt.Errorf("can't get retrieve about allocated pools")
-	}
-	defer rows.Close()
-
-	allocatedPools := []string{}
-	for rows.Next() {
-		var name string
-		if err := rows.Scan(&name); err != nil {
-			return fmt.Errorf("illegal pool name retrieved: %w", err)
-		}
-		allocatedPools = append(allocatedPools, name)
-	}
-	if err = rows.Err(); err != nil {
-		return fmt.Errorf("can't get allocated pool names: %w", err)
-	}
-
-	for _, name := range allocatedPools {
-		if _, exists := pool[name]; exists {
-			pool[name] = true
-		} else {
-			log.Logger.Warn("Incorrect pool name found", "warning", name)
-			continue
-		}
-	}
-
-	poolName := make([]string, 0, len(pool))
-	for k, v := range pool {
-		if !v {
-			poolName = append(poolName, k)
-		}
-	}
-
-	if len(poolName) == 0 {
-		return fmt.Errorf("no available pools")
-	}
-
-	sb.SubscriptionName = poolName[rand.Intn(len(poolName))]
-	return sb.Save()
-}
-
 // func (sb *AzureSandboxWithCreds) Create() {
 // 	// TODO: Implement provisioning here
 // 	poolClient := azure.InitPoolClient(
@@ -455,12 +467,6 @@ func (sb *AzureSandboxWithCreds) allocatePool() error {
 // ////////////////////// MOCKUPS ////////////////////////
 // TODO: this mockup code shoul be deleted
 func (sb *AzureSandboxWithCreds) Create() {
-	err := sb.allocatePool()
-	if err != nil {
-		log.Logger.Error("Can't allocate pool for new Azure Sandbox", "error", err)
-		sb.setStatus("error")
-		return
-	}
 
 	// This is a just mockup, normally it should be filled
 	// by the code whichi really create sandbox
@@ -501,7 +507,7 @@ func (sb *AzureSandboxWithCreds) Create() {
 	}
 
 	sb.Status = "success"
-	err = sb.Save()
+	err := sb.Save()
 	if err != nil {
 		log.Logger.Error("Can't update Azure Sandbox status", "error", err)
 		return
@@ -530,30 +536,3 @@ func (sb *AzureSandboxWithCreds) Delete() error {
 }
 
 //////////////////////// MOCKUPS ////////////////////////
-
-// - Create simple "pool" function that return random poolID between pool-01-001 - pool-01-010
-// - Create mockups function that will return BS date to store at the database.
-//   The important part here is to get poolID from the the "pool" function created above.
-// - Update the "pool" function to get existed pools from the database.
-// - Update the "pool" function to return only free pools
-// - Update the "pool" function to make it single treaded. This probably will require some global
-//   items like locks or workgroups.
-
-// We have two places that we need to block:
-// - allocate pool and save it to the database
-// - release pool and remove it from the database
-
-// My thoughts are:
-
-// Create two functions
-// - AllocatePool. Get pointer to sandbox structure as parameter, check for "free"
-//   sandbox in the database, assign it or return error and save sandbox info to database.
-// - ReleasePool. Also getting sandbox structure as a parameter, set sandbox as nil and
-//   save it to database.
-
-// Get list of subscription names from the database with: select resource_data ->> 'subscription_name' as subscription_name from resources;
-// poolSize := 10
-
-// for i := 1; i <= poolSize; i++ {
-// 	fmt.Printf("%03d\n", i)
-//}
