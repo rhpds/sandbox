77e1fc4fdcae85a9f0c6d396b56ac5690592d48d
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-02-21 02:02:32 +0100

    Update jenkins-run.sh

--- tests/jenkins-run.sh
+++ tests/jenkins-run.sh
@@ -137,8 +137,8 @@ for payload in sandbox-api-configs/dns-accounts-configs/dev*.json; do
             bitwarden/bws:0.5.0 secret list  $BWS_PROJECT_ID \
             | KEYVALUE="${account}.secret_access_key" jq -r '.[] | select(.key==env.KEYVALUE) | .value')
 
-    jq  --arg access_key_id $ACCESS_KEY_ID --arg secret_access_key $SECRET_ACCESS_KEY '(.aws_access_key_id = $access_key_id | .aws_secret_access_key = $secret_access_key)'  < $payload > $payload2
-
+    jq  --arg access_key_id $ACCESS_KEY_ID --arg secret_access_key $SECRET_ACCESS_KEY '(.aws_access_key_id = $access_key_id | .aws_secret_access_key = $secret_access_key)'  < $payload > $payload2 
+    cat $payload2
     # In bash, files in a * are sorted alphabetically by default
     # so a create will always happen before an update.
     if [[ $payload =~ create.json$ ]]; then
