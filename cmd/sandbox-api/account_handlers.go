96a8bf99dfd708af668ed947db532f3052237610
Guillaume Coré <gucore@redhat.com>
2026-01-05 10:56:38 +0100

    Add resource type to GetStatusAccountHandler

--- cmd/sandbox-api/account_handlers.go
+++ cmd/sandbox-api/account_handlers.go
@@ -481,34 +481,73 @@ func (h *BaseHandler) LifeCycleAccountHandler(action string) http.HandlerFunc {
 func (h *BaseHandler) GetStatusAccountHandler(w http.ResponseWriter, r *http.Request) {
 	// Grab the parameters from Params
 	accountName := chi.URLParam(r, "account")
+	kind := chi.URLParam(r, "kind")
 
-	// We don't need 'kind' param for now as it is checked and validated
-	// by the swagger openAPI spec.
+	var job *models.LifecycleResourceJob
+	var err error
 
-	// Get the account from DynamoDB
-	sandbox, err := h.awsAccountProvider.FetchByName(accountName)
-	if err != nil {
-		if err == models.ErrAccountNotFound {
-			log.Logger.Warn("GET account", "error", err)
-			w.WriteHeader(http.StatusNotFound)
+	switch kind {
+	case "AwsSandbox", "aws":
+		// Get the account from DynamoDB
+		sandbox, fetchErr := h.awsAccountProvider.FetchByName(accountName)
+		if fetchErr != nil {
+			if fetchErr == models.ErrAccountNotFound {
+				log.Logger.Warn("GET account", "error", fetchErr)
+				w.WriteHeader(http.StatusNotFound)
+				render.Render(w, r, &v1.Error{
+					HTTPStatusCode: http.StatusNotFound,
+					Message:        "Account not found",
+				})
+				return
+			}
+			log.Logger.Error("GET account", "error", fetchErr)
+
+			w.WriteHeader(http.StatusInternalServerError)
 			render.Render(w, r, &v1.Error{
-				HTTPStatusCode: http.StatusNotFound,
-				Message:        "Account not found",
+				HTTPStatusCode: http.StatusInternalServerError,
+				Message:        "Error reading account",
 			})
 			return
 		}
-		log.Logger.Error("GET account", "error", err)
 
-		w.WriteHeader(http.StatusInternalServerError)
+		// Get the last saved status for that account
+		job, err = sandbox.GetLastStatus(h.dbpool)
+
+	case "OcpSandbox", "ocp":
+		// Get the OCP sandbox
+		sandbox, fetchErr := h.OcpSandboxProvider.FetchByName(accountName)
+		if fetchErr != nil {
+			if fetchErr == models.ErrAccountNotFound {
+				log.Logger.Warn("GET account", "error", fetchErr)
+				w.WriteHeader(http.StatusNotFound)
+				render.Render(w, r, &v1.Error{
+					HTTPStatusCode: http.StatusNotFound,
+					Message:        "Account not found",
+				})
+				return
+			}
+			log.Logger.Error("GET account", "error", fetchErr)
+
+			w.WriteHeader(http.StatusInternalServerError)
+			render.Render(w, r, &v1.Error{
+				HTTPStatusCode: http.StatusInternalServerError,
+				Message:        "Error reading account",
+			})
+			return
+		}
+
+		// Get the last saved status for that account
+		job, err = sandbox.GetLastStatus(h.dbpool)
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
 		render.Render(w, r, &v1.Error{
-			HTTPStatusCode: http.StatusInternalServerError,
-			Message:        "Error reading account",
+			HTTPStatusCode: http.StatusBadRequest,
+			Message:        fmt.Sprintf("Unknown account kind: %s", kind),
 		})
 		return
 	}
 
-	// Get the last saved status for that account
-	job, err := sandbox.GetLastStatus(h.dbpool)
 	if err != nil {
 		// Check no row
 		if err == pgx.ErrNoRows {
